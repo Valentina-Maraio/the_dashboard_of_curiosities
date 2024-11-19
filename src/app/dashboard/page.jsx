762da331c24f52f07ac49e77713b93bc160ad90b
Valentina Maraio <valentina.maraio89@gmail.com>
2024-11-19 12:08:39 +0100

    generate dashboard structure plus thre charts

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,70 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { DetailedRoundChart } from "@/components/detailed-round-chart";
+import { LabelChart } from "@/components/label-chart";
+import { RadarChart, SimpleRadarChart } from "@/components/simple-radar-chart";
+import { RoundChart } from "@/components/round-chart";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { SegmentChart } from "@/components/segment-chart";
+import { SemiChart } from "@/components/semi-chart";
+
+export default function Page() {
+  return (
+    (<SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">
+                  Building Your Application
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-l bg-gray-100/50 dark:bg-gray-800/50">
+            <RoundChart/>
+            </div>
+            <div className="aspect-video rounded-l bg-gray-100/50 dark:bg-gray-800/50">
+            <SemiChart/>
+            </div>
+            <div className="aspect-video rounded-l bg-gray-100/50 dark:bg-gray-800/50">
+            <SegmentChart/>
+            </div>
+          </div>
+          <div
+            className="min-h-[100vh] flex-1 rounded-xl bg-gray-100/50 md:min-h-min dark:bg-gray-800/50" />
+          {Array.from({ length: 1 }).map((_, index) => (
+            <div
+              key={index}
+              className="aspect-video h-12 w-full rounded-lg bg-muted/50"
+            >
+            </div>
+          ))}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>)
+  );
+}
